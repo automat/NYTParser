c5983fe480bd1b096e266a58049c5503a8ff89aa
Henryk Wollik <olafyoorsen@hotmail.com>
2012-09-27 11:21:03 +0200

    Initial Commit

---
+++ NYTParser/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NYTParser
+//
+//  Created by Henryk on 27.09.12.
+//  Copyright (c) 2012 Henryk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
